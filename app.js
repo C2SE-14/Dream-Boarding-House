11ae8b1945b9e4dcb8507f80e57c5cacee354872
Huong8526 <dung.nguyen@smartdev.com>
2022-03-29 15:59:57 +0700

    initial v0.0.1

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set("view engine", "ejs");
 
 app.use(cors());
 // app.use(logger("dev"));
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const registerRouter = require("./register");
-
+const authRouter = require("./auth")
 function route(app) {
   app.use("/register", registerRouter);
+  app.use("/login", authRouter);
 }
 
 module.exports = route;
