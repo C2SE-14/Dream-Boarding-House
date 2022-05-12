1bb94adc7e68191d0bf5d1176156cff1a90fd627
Dung Nguyen Anh <dung.nguyen@smartdev.com>
2022-05-12 18:25:42 +0700

    manage user information

--- controllers/favorite-list.controller.js
+++ controllers/favorite-list.controller.js
@@ -1,18 +1,22 @@
-const favoriteRoom = require('../models/favoriteRoom.modal')
+const favoriteRoom = require('../models/favoriteRoom.modal');
 const Room = require('../models/room.modal');
 const getFavoriteList = async(req, res, next) => {
     try {
-        const userId = req.cookies.user.user_id;
+        let userId = '';
+        if(req.cookies.user) {
+            userId = req.cookies.user.user_id;
+        }
         const listRoomId = await favoriteRoom.find({userId: userId});
         const listFavoriteRoom = [];
         for(let i = 0; i < listRoomId.length; i++) {
             const myFavoriteRoom = await Room.findOne({_id: listRoomId[i].roomId});
             listFavoriteRoom.push(myFavoriteRoom);
         }
+        console.log('list favorite room: ', listFavoriteRoom);
         res.render("favoriteList", {title: "favorite room", listFavoriteRoom})
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: error });
+        res.status(203).json({ message: error });
     }
 }
 
@@ -42,17 +46,28 @@ const deleteFavoriteList = async(req, res, next) => {
     try {
         // const userId = req.cookies.user._id;
         const myRoomId = req.params;
-        console.log('roomId to delete: ', roomId);
+        console.log('my room id: ', myRoomId);
         await favoriteRoom.deleteOne({ roomId: myRoomId })
         .then(() => {
         console.log('delete successfully')
-        res.status(200).redirect("/");
         })
         .catch((error) => {
         res.status(500).send(error);
         });
+
+        let userId = '';
+        if(req.cookies.user) {
+            userId = req.cookies.user.user_id;
+        }
+        const listRoomId = await favoriteRoom.find({userId: userId});
+        const listFavoriteRoom = [];
+        for(let i = 0; i < listRoomId.length; i++) {
+            const myFavoriteRoom = await Room.findOne({_id: listRoomId[i].roomId});
+            listFavoriteRoom.push(myFavoriteRoom);
+        }
+        res.status(200).render("favoriteList", {title: "Dream Boarding House", listFavoriteRoom})
     } catch (error) {
-        res.status(500).json({ "message": error });
+        res.status(500).json({ "message: ": error });
         console.log('error: ', error);
     }
 }
---
+++ controllers/user.controller.js
@@ -0,0 +1,20 @@
+const { response } = require('express');
+const User = require('../models/user.model');
+const getUserInformation = async (req, res, next) => {
+    try {
+        let userId = req.cookies.user.user_id;
+        let user;
+        if(!userId) {
+            user = req.cookies.user.user_id;
+        }
+        const userInfor = await User.find({_id: userId});
+        console.log('userInfor: ', userInfor)
+        res.status(200).render('userInformation', {title: 'Dream boarding house', userInfor, user});
+    } catch(error) {
+        console.log(error);
+        //res.status(203).render('error')
+    }
+}
+module.exports = {
+    getUserInformation,
+}
