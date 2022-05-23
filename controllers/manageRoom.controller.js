c06d15d66b6825f8743bc4f6ff92f34da99c5ba7
Juhu <nguyenanhdung462@gmail.com>
2022-05-23 13:19:47 +0700

    improve UI

--- controllers/manageRoom.controller.js
+++ controllers/manageRoom.controller.js
@@ -32,7 +32,37 @@ const getMyRoom = async (req, res, next) => {
 
     }
 }
+const deleteMyRoom = async (req, res, next) => {
+    try {
+        const roomId = req.params.id;
+        const user = req.cookies.user;
+        let userId, role, showSearch = "no";
+        if(user) {
+            userId = req.cookies.user.user_id
+            role = await Role.findOne({userId: userId});
+            role = role.name;
+        }
+        let numberNotification = await NotificationService.getNumberNotification(userId);
+        await Room.deleteOne({
+            userId: userId,
+            id: roomId
+        })
+        let perPage = 4;
+        let page = req.params.page || 1;
+        await Room.find({userId: userId}).skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec((err, listRoom) => {
+            Room.countDocuments((err, count) => {
+                if(err) return next(err);
+                res.status(200).render("manageRoom", {title: "Dream Boarding House", listRoom, current: page, pages: Math.ceil(count / perPage), user, role, listRoom, showSearch, numberNotification})
 
+            })
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
 module.exports = {
     getMyRoom,
+    deleteMyRoom
 }
--- routes/manageRoom.js
+++ routes/manageRoom.js
@@ -6,5 +6,5 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/myRoom/all", manageRoomController.getMyRoom);
-
+router.post("/myRoom/delete/:id", manageRoomController.deleteMyRoom);
 module.exports = router;
