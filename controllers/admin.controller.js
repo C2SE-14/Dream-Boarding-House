3fb3a975ef9f03e75beef2c9ca040b9f6bea8ec9
Juhu <nguyenanhdung462@gmail.com>
2022-05-24 08:45:22 +0700

    improve UI

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,17 +1,26 @@
 const User = require("../models/user.model");
 const Role = require("../models/role.model");
+const Room = require("../models/room.model");
 
 const renderAdminPage = async (req, res, next) => {
+  const renderUsers = await User.find({ deleted: false });
+  const post = await Room.find({isAccept: true});
+  const blockUser = await User.find({deleted: true});
+  const numOfUser = renderUsers.length;
+  const numOfPost = post.length;
+  const numOfBlock = (numOfUser - blockUser.length);
   res.render("admin.template/master", {
     title: "Dashboard Admin",
-    content: "../admin.template/main_content",
+    content: "../admin.template/main_content", numOfUser, numOfPost, numOfBlock
   });
 };
 
 const getAllUsers = async (req, res, next) => {
   const renderUsers = await User.find({ deleted: false });
-  console.log(renderUsers);
-  const roleUserId = await Role.find();
+  renderUsers.forEach((item, index) => {
+    if(item.username === "admin1") renderUsers.splice(index, 1);
+  })
+  const roleUserId = await Role.find({$or:[ {'name':"Người thuê trọ"}, {'name':"Chủ phòng trọ"} ]});
   res.render("admin.template/master", {
     title: "Dashboard Admin",
     content: "../admin.page/users",
@@ -19,7 +28,19 @@ const getAllUsers = async (req, res, next) => {
     roleUserId,
   });
 };
-
+const getAllPosts = async(req, res, next) => {
+  try {
+    const post = await Room.find();
+    console.log('post', post);
+    res.render("admin.template/master", {
+      title: "Dashboard Admin",
+      content: "../admin.page/posts",
+      post
+    });
+  } catch (error) {
+    console.log('error: ', error);
+  }
+}
 const blockUser = async (req, res, next) => {
   const block = await User.delete({ _id: req.params.id })
     .then(() => {
@@ -34,4 +55,5 @@ module.exports = {
   renderAdminPage,
   getAllUsers,
   blockUser,
+  getAllPosts,
 };
